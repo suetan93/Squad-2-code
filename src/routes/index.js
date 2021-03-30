b3f6a1b65d63f662f486025967ada52ccc936b2d
Sina-Programs <nikmaramcareer@gmail.com>
2021-03-30 15:29:04 -0400

    Reset Source Code Repo

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo March 12th 2021' });
-});
-
-module.exports = router;
