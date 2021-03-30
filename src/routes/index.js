366abb42c33e8a3d2178336f60dee77db456483b
Sina-Programs <nikmaramcareer@gmail.com>
2021-03-30 15:14:37 -0400

    Test Change and Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo March 12th 2020' });
+  res.render('index', { title: 'Cloud Native demo March 12th 2021' });
 });
 
 module.exports = router;
