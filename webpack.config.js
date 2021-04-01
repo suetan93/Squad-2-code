90ed632c652f19cb01f6476d164e22b84e0bbe13
suetan93 <suetan93@gmail.com>
2021-04-01 10:03:26 -0700

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = {
-  entry: './client/index.js',
+  entry: path.join(__dirname, '/client/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
